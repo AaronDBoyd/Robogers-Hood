a00e5da7a91eeb7103950a6ad7cebee998f31b1c
Aaron Boyd <a.boyd91@yahoo.com>
2022-02-10 19:51:17 -0800

    add and pass the first test.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//Business Logic
+
+const userNum = 78;
+const numToStrng = userNum.toString();
+let numArray = [];
+
+
+
+for ( let i = 0; i <= userNum ; i++) {
+  numArray.push(i);
+}
+ console.log(numArray);
