2cd206d05964c29cb97249d83debf334f4700f78
Aaron Boyd <a.boyd91@yahoo.com>
2022-02-11 18:38:09 -0800

    add and pass fifth test for business logic.

--- README.md
+++ README.md
@@ -4,14 +4,29 @@ Test: "It should return an array of numbers from 0 to the user's inputted number
 Code: beepBoop(5);
 Expected Output: [0, 1, 2, 3, 4, 5]
 
+---
+
 Test: "It will recognize any number that contains a 3 and change the entire digit to the string 'Won't you be my neighbor?.'"
 Code: beepBoop(5);
 Expected Output: [0, 1, 2, "Won't you be my neighbor?", 4, 5]
+
+---
 
 Test: "It will recognize any number that contains a 2 and change the entire digit to the string to 'Boop!' unless it contains a 3."
 Code: beepBoop(23);
 Expected Output: [0, 1, "Boop!", "Won't you be my neighbor?", 4, 5, 6, 7, 8, 9, 10, 11, "Boop!", "Won't you be my neighbor?", 14, 15, 16, 17, 18, 19, "Boop!", "Boop!", "Boop!", "Won't you be my neighbor?"]
 
+---
+
 Test: "It will any number that contains a 1 and change the entire digit to the string 'Beep!' unless it contains a 2 or a 3."
 Code: beepBoop(13);
 Expected Output: [0, "Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, 6, 7, 8, 9, "Beep!", "Beep!", "Boop!", "Won't you be my neighbor?"]
+
+---
+
+Test: "It will recognize an input that isn't a number and return a message to the user to enter a number."
+Code: beepBoop("Number");
+Expected Output: "Please enter a number"
+
+---
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,28 @@
 //Business Logic
 
 const userNum = 23;
-const numToStrng = [];
-let roboArray = [];
 
-// function beepBoop() {
 
-for ( let i = 0; i <= userNum ; i++) {
-  // roboArray.push(i);
-  if(i.toString().includes(3)) {
-    roboArray.push("Won't you be my neighbor?")
-  }else if (i.toString().includes(2)) {
-    roboArray.push("Boop!")
-  }else if (i.toString().includes(1)) {
-    roboArray.push("Beep!")
-  }else {
+
+function beepBoop(userNum) {
+  const roboArray = [];
+  if(isNaN(userNum)) {
+    roboArray.push("Please insert a *bleeping* NUMBER!")
+  } else{
+    for ( let i = 0; i <= userNum ; i++) {
+      if(i.toString().includes(3)) {
+        roboArray.push("Won't you be my neighbor?")
+      }else if (i.toString().includes(2)) {
+        roboArray.push("Boop!")
+      }else if (i.toString().includes(1)) {
+        roboArray.push("Beep!")
+      }else {
     roboArray.push(i);
-  }
-}
-// return roboArray;
+      }
+    }
+    return roboArray;
+  }}; 
 
 
 
-
-// numArray.forEach(function(number) {
-//   numToStrng.push(number.toString());
- 
-// })
-
-// numToStrng.forEach(function(strng) {
-//   if (strng.includes("3")) {
-//     numToString.replace(strng, "Won't you be my neighbor?");
-//   }
-// })
-
-// const neighbor = numArray.indexOf(3);
 
